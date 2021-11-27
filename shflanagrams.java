67f29be2e9c627bd57c5dd5ef445d9545350130b
samratpodder <iamsamrat@outlook.com>
2021-11-28 00:19:17 +0530

    Unfinished Shuffle Anagrams added

---
+++ shflanagrams.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+public class shflanagrams {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int tc = sc.nextInt();
+        int t =1;
+        while (tc>0) {
+            tc--;
+            String str = sc.next();
+            sc.nextLine();
+            
+        }
+    }
+}
